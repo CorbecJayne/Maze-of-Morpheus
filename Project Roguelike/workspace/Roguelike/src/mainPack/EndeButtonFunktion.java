a38e0db5c6cb0290fa192b51921f246b8e0f8a7f
CorbecJayne <corbec.jayne@gmail.com>
2015-06-14 12:33:37 +0200

    added source code

---
+++ Project Roguelike/workspace/Roguelike/src/mainPack/EndeButtonFunktion.java
@@ -0,0 +1,18 @@
+package mainPack;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class EndeButtonFunktion implements ActionListener{
+
+	
+	public void actionPerformed(ActionEvent arg0) {
+		//Schlieﬂe Programm
+		System.exit(0);
+		
+	}
+	
+	
+
+}
---
+++ Project Roguelike/workspace/Roguelike/src/mainPack/StartButtonFunktion.java
@@ -0,0 +1,20 @@
+package mainPack;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class StartButtonFunktion implements ActionListener{
+
+	
+	public void actionPerformed(ActionEvent arg0) {
+		//Startet Spiel
+		@SuppressWarnings("unused")
+		Client client = new Client();
+		
+		
+	}
+	
+	
+
+}
